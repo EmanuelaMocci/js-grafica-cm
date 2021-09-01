f0d41a52fa54da90d43fc7d2164b3c0e16bc03ef
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-01 14:29:35 +0200

    Aggiunta prompt

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
+var celle = prompt("Inserisci il numero di celle desiderate");
+
+// Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
+
+
+// Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
