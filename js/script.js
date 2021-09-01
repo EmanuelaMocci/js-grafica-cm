2887421f2725226689535ab93fc172bd760d1aa3
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-01 17:21:27 +0200

    Aggiunta funzione con ciclo for

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-// Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
-var celle = prompt("Inserisci il numero di celle desiderato");
 
 // Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
 // Crea un ciclo for per ripetere le celle
-for (var i = 2; i <= celle; i++) {
-    document.getElementById("contenitore").innerHTML += `<div class="quadrato">${i}</div>`;
+
+function caselle(celle) {
+
+    for (var i = 1; i <= celle; i++) {
+        document.getElementById("contenitore").innerHTML += `<div class="quadrato">${i}</div>`;
+    }
 }
+
+// Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
+var celleUtente = prompt("Inserisci il numero di celle desiderato");
+
+caselle(celleUtente);
 
 // Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
 // Crea il click con la funzione
