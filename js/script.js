61512926cc89e9c5ce95a7ab8c7f7d1cb066df90
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-01 15:08:33 +0200

    Aggiunta contatore

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 // Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
-var celle = prompt("Inserisci il numero di celle desiderate");
+var celle = prompt("Inserisci il numero di celle desiderato");
 
 // Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
+// Crea un ciclo for per ripetere le celle
+for (var i = 1; i <= celle; i++) {
+    document.getElementById("contenitore").innerHTML += `<div class="quadrato">${i}</div>`;
+}
 
 
 // Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
+// Crea il click con la funzione
